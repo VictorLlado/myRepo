e16ac5a8be84cc32507957abfe1a19ef1d642679
VictorLlado <40065303+VictorLlado@users.noreply.github.com>
2018-06-11 17:30:49 +0200

    Create result.php

---
+++ result.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html> 
+<html> 
+	<head> 
+		<title>Result page</title> 
+		
+<style type="text/css">
+.results {margin-left:12%; margin-right:12%; margin-top:10px;}
+</style>
+	</head> 
+	
+	
+<body bgcolor="#6191ff"> 
+
+<form action="result.php" method="get"> 
+		
+		<span><b>Write your Keyword:</b></span>
+		
+		<input type="text" name="user_keyword" size="120"/> 
+		<input type="submit" name="result" value="Search Now">
+</form>
+	
+	<a href="search.html"><button>Go Back</button></a>
+	
+<?php 
+	$db = new mysqli;
+    $db->connect('localhost','root','anacardoVolante','search');
+    if (!$db) {
+    	echo "success";
+    	exit();
+    }
+	
+	$get_value = $_GET['result.php'];	
+	if($get_value==''){
+	echo "<center><b>Please go back, and write something in the search box!</b></center>";
+	exit();
+	}
+	
+	$result_query = "SELECT * FROM sites";
+	$run_result = mysqli_query($result_query);
+	if(mysqli_num_rows($run_result)){
+	echo "<center><b>Oops! sorry, nothing was found in the database!</b></center>";
+	exit();
+	}
+
+	while($row_result=mysqli_fetch_array($run_result)){	
+		  $site_title=$row_result['site_title'];
+		  $site_link=$row_result['site_link'];
+		  $site_desc=$row_result['site_desc'];
+		  $site_image=$row_result['site_image'];
+	
+	echo "<div class='results'>
+		 <h2>$site_title</h2>
+		 <a href='$site_link' target='_blank'>$site_link</a>
+		 <p align='justify'>$site_desc</p> 
+		 <img src='images/$site_image' width='100' height='100' />
+		 </div>";
+		}
+	
+?>
+
+</body> 
+</html>
+    
